8e8e54c23c34799623731d8578cb8639d6b0d17b
Stageddat <chmarc72@gmail.com>
2025-01-25 20:09:16 +0100

    feat: update Discord URL in socialLinks data for accuracy

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,20 +1,20 @@
 import type { SocialLink } from "../types/SocialLink"
 
 export const socialLinks: SocialLink[] = [
-  {
-    name: "GitHub",
-    url: "https://github.com/stageddat",
-    icon: "icon-[simple-icons--github]",
-  },
-  {
-    name: "YouTube",
-    url: "https://youtube.com/@stageddat",
-    icon: "icon-[simple-icons--youtube]",
-  },
-  {
-    name: "Discord",
-    url: "https://discord.gg/stageddat",
-    icon: "icon-[simple-icons--discord]",
-  },
+    {
+        name: "GitHub",
+        url: "https://github.com/stageddat",
+        icon: "icon-[simple-icons--github]",
+    },
+    {
+        name: "YouTube",
+        url: "https://youtube.com/@stageddat",
+        icon: "icon-[simple-icons--youtube]",
+    },
+    {
+        name: "Discord",
+        url: "https://discord.com/users/756509638169460837",
+        icon: "icon-[simple-icons--discord]",
+    },
 ]
 
