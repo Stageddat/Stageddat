651d10cd1d3461b5d6d858be2d71c7db270e7290
Stageddat <chmarc72@gmail.com>
2025-01-25 17:23:18 +0100

    init: website

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion"
+
+interface Props {
+  text: string
+  className?: string
+  delay?: number
+}
+
+export default function AnimatedText({ text, className = "", delay = 0 }: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.7, delay }}
+      className={className}
+    >
+      {text}
+    </motion.div>
+  )
+}
+
---
+++ src/data/socialLinks.ts
@@ -0,0 +1,20 @@
+import type { SocialLink } from "../types/SocialLink"
+
+export const socialLinks: SocialLink[] = [
+  {
+    name: "GitHub",
+    url: "https://github.com/stageddat",
+    icon: "icon-[simple-icons--github]",
+  },
+  {
+    name: "YouTube",
+    url: "https://youtube.com/@stageddat",
+    icon: "icon-[simple-icons--youtube]",
+  },
+  {
+    name: "Discord",
+    url: "https://discord.gg/stageddat",
+    icon: "icon-[simple-icons--discord]",
+  },
+]
+
